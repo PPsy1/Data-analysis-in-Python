19e7eba7a5aff4df5c0e17da940ab43f1a1c34a2
PPsy1 <37116498+PPsy1@users.noreply.github.com>
2018-12-22 12:54:15 +0200

    Add files via upload
    Automated file creation for specific analysis to find delivery dates using Pandas/Python

---
+++ stats.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+
+import os 
+import pandas as pd
+from tkinter import filedialog 
+
+pd.set_option('display.max_rows', 100000)
+pd.set_option('display.expand_frame_repr', False)
+
+fname = filedialog.askopenfilename(title="Select file: ")
+
+with open(os.path.dirname(fname) + '/'+'statistics.txt', 'w') as fw: 
+    csv_f = pd.read_csv(fname, sep=",",   #read csv
+                  encoding = 'utf-8',parse_dates=['del_day'], #delivery date
+                  usecols=['del_day','user','code_paper','number_of_files'],
+                  dayfirst=True)
+    csv_f = csv_f[csv_f['code_paper'].notnull()]
+
+
+    fi = csv_f.groupby(['del_day','user']).agg({'del_day' : 'count','number_of_files':'sum'}).sort_index(level=[0,1], ascending=[False,True]))
+    fw.write(str(fi))
